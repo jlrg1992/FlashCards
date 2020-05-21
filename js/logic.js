5bbc7aeaae57cc0e74f2d58a9f652cda1d04f2a5
jlrg1992 <jlrg1992@gmail.com>
2020-05-21 17:15:26 -0500

    Primera parte

---
+++ js/logic.js
@@ -0,0 +1,37 @@
+let preguntas; 
+let count;
+
+
+function displayListElement(item){
+    str = '<div class="post-preview"><h2 class="post-title">' + item.pregunta + '</h2><p class="post-meta">' + item.respuesta + '</p></div><hr>'
+    $("#lista").append(str);
+}
+function displayLista(lista){
+    for(lista of lista){
+        displayListElement(lista);
+    }
+}
+function getPreguntas(deck){
+    $.getJSON(deck, function(r){
+        displayLista(r);
+        preguntas = r;
+        count = 0;
+        displayQuestion(preguntas[count]);
+    });
+}
+function startPage(){
+    let working = getPreguntas("json/test.json");
+    return working
+}
+function displayQuestion(question){
+    $("#main-pregunta").html(question.pregunta);
+    $("#main-respuesta").hide();
+    $("#main-respuesta").html(question.respuesta);
+}
+function siguientePregunta(){
+    if((count+=1)>=preguntas.length){
+    count = 0;
+    }
+    displayQuestion(preguntas[count]);
+}
+startPage();
